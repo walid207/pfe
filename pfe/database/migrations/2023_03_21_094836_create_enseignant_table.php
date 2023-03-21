909959b517e29c4fc59c885ab3a8e7827787bf2b
walid207 <djerbien207@gmail.com>
2023-03-21 22:21:30 +0100

    copie0

---
+++ pfe/database/migrations/2023_03_21_094836_create_enseignant_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enseignant', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string('mot de passe');
+            $table->string('specialite');
+            $table->string('repartition horaire');
+            $table->string('niveaux');
+            $table->string('fiche de paie');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enseignant');
+    }
+};
